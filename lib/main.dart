18919e78e3ed344d110b340f5fef32f0b43e7535
aseemmeenan <aseemmeenan@gmail.com>
2022-02-10 00:26:08 +0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:chat_app/pages/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(primaryColor: Colors.red.shade900),
+    home: HomePage(),
+  ));
+}
